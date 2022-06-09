2c6d9a55c5ee333c14edb9ae41fa8ab30369a177
GayathriNarayan <gayathrinarayan807@gmail.com>
2022-06-09 10:49:13 +0200

    Added updated base and home files
    Added updated base and home files

--- webshop/onlineapp/views.py
+++ webshop/onlineapp/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+  return render(request,'onlineapp/home.html')
+
+
+def clothing(request):
+  context = {}
+  return render(request,'onlineapp/clothing.html', context)
+
+
+def shoes(request):
+  context = {}
+  return render(request,'onlineapp/shoes.html', context)
+
+def accessories(request):
+  context = {}
+  return render(request,'onlineapp/accessories.html', context)
