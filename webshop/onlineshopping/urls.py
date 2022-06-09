d7ae7fb143d5ec2c39edb273b1530fba226ef422
GayathriNarayan <gayathrinarayan807@gmail.com>
2022-06-09 10:32:46 +0200

    Added home and base updated files
    Added home and base updated files

--- webshop/onlineshopping/urls.py
+++ webshop/onlineshopping/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from onlineapp import views
+
+app_name = 'onlineapp'
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('',include('onlineapp.urls')),
-    path('', views.home),
+    path('',include('onlineapp.urls')),
+    path('', views.home, name="home"),
 ]
